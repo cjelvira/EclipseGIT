37e69eba54953489e3462a0eebba039e7f60e942
Carlos <carlosjose.elvira@universidadeuropea.es>
2023-03-08 19:25:52 +0100

    Primer commit desde Rama2

--- src/EclipseGIT/EclipseGIT.java
+++ src/EclipseGIT/EclipseGIT.java
@@ -4,5 +4,6 @@ public class EclipseGIT {
 	public static void main(String[] args) {
 		System.out.println("Primer comando Eclipse GIT");
 		System.out.println("Segundo comando Eclipse GIT");
+		System.out.println("Primer comando desde Rama2");
 	}
 }
