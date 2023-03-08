8a113d0c7578535ffc841f8ff901794d00545819
Carlos <carlosjose.elvira@universidadeuropea.es>
2023-03-08 19:05:31 +0100

    Segundo commit

--- src/EclipseGIT/EclipseGIT.java
+++ src/EclipseGIT/EclipseGIT.java
@@ -1,9 +1,8 @@
 package EclipseGIT;
 
 public class EclipseGIT {
-
 	public static void main(String[] args) {
 		System.out.println("Primer comando Eclipse GIT");
+		System.out.println("Segundo comando Eclipse GIT");
 	}
-
 }
